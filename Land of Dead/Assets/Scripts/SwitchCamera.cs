5366d4782bdf63dd1319f423525c873b2af483b6
mrideedy <bca40584.20@bitmesra.ac.in>
2022-12-06 10:16:02 +0530

    initial commit21

---
+++ Land of Dead/Assets/Scripts/SwitchCamera.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+   [Header("Camera to Assign")]
+   public GameObject AimCam;
+   public GameObject AimCanvas;
+   public GameObject ThirdPersonCam;
+   public GameObject ThirdPersonCanvas;
+
+    private void Update() 
+    {
+        if(Input.GetButton("Fire2"))
+        {
+            ThirdPersonCam.SetActive(false);
+            ThirdPersonCanvas.SetActive(false);
+            AimCam.SetActive(true);
+            AimCanvas.SetActive(true);
+        }
+        else
+        {
+            ThirdPersonCam.SetActive(true);
+            ThirdPersonCanvas.SetActive(true);
+            AimCam.SetActive(false);
+            AimCanvas.SetActive(false);
+        }
+    }
+}
