a8d66d2f7530af9251f288deff759a412fa0652d
Rishabh Shankar <rishabhshankar05@gmail.com>
2022-12-06 14:04:06 +0530

    initial commit 25

--- Land of Dead/Assets/Scripts/Rifle.cs
+++ Land of Dead/Assets/Scripts/Rifle.cs
@@ -10,13 +10,35 @@ public class Rifle : MonoBehaviour
     public float shootingRange = 80f;
     public float fireCharge = 15f;
     private float nextTimeToShoot = 0f;
+    public PlayerScript player;
 
+
+    [Header("Rifle Ammunition and shooting")]
+    private int maximumAmmunition = 32;
+    public int mag = 10;
+    private int presentAmmunition;
+    public float reloadingTime = 1.3f;
+    private bool setReloading = false;
 
     [Header("Rifle Effects")]
     public ParticleSystem muzzleSpark;
     public GameObject WoodedEffect;
 
+    private void Awake(){
+        presentAmmunition = maximumAmmunition;
+
+    }
+
     private void Update() {
+        if(setReloading){
+            return;
+        }
+
+        if(presentAmmunition==0){
+            StartCoroutine(Reload());
+            return;
+        }
+
         if(Input.GetButton("Fire1") && Time.time >= nextTimeToShoot)
         {
             nextTimeToShoot = Time.time + 1f/fireCharge;
@@ -26,6 +48,20 @@ public class Rifle : MonoBehaviour
 
     private void Shoot()
     {
+        //check for mag
+        if(mag==0){
+            //show ammo out text
+            return;
+        }
+        
+        presentAmmunition--;
+
+        if(presentAmmunition==0){
+            mag--;
+        }
+
+        //Updating the UI
+
         muzzleSpark.Play();
         RaycastHit hitInfo;
         if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, shootingRange))
@@ -41,4 +77,22 @@ public class Rifle : MonoBehaviour
             }
         }
     }
+    
+    IEnumerator Reload(){
+        player.playerSpeed = 0f;
+        player.playerSprint = 0f;
+        setReloading = true;
+        Debug.Log("Reloading...");
+        //play animation
+        //play reload sound
+        yield return new WaitForSeconds(reloadingTime);
+        //play animation
+        presentAmmunition = maximumAmmunition;
+        player.playerSpeed = 1.9f;
+        player.playerSprint = 3;
+        setReloading = false;
+    }
+
+
+
 }
