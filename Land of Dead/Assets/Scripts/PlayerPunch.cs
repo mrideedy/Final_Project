33ea6f67d7ea537c78b44cf77871c7c38878176a
Aditya <aadi.avi990@gmail.com>
2022-12-08 16:17:46 +0530

    Initial Commit 39
    Added Zombie Script and Animation.
    Added damage to 2nd zombie by punch and gun.

--- Land of Dead/Assets/Scripts/PlayerPunch.cs
+++ Land of Dead/Assets/Scripts/PlayerPunch.cs
@@ -21,6 +21,7 @@ public class PlayerPunch : MonoBehaviour
 
             ObjectToHit objectToHit = hitInfo.transform.GetComponent<ObjectToHit>();
             Zombie1 zombie1 = hitInfo.transform.GetComponent<Zombie1>();
+            Zombie2 zombie2 = hitInfo.transform.GetComponent<Zombie2>();
 
             if(objectToHit != null){
                 objectToHit.ObjectHitDamage(giveDamageOf);
@@ -29,6 +30,10 @@ public class PlayerPunch : MonoBehaviour
             else if(zombie1 != null){
                 zombie1.zombieHitDamage(giveDamageOf);
                 
+            }
+            else if(zombie2 != null)
+            {
+                zombie2.zombieHitDamage(giveDamageOf);
             }
         }
     }
--- Land of Dead/Assets/Scripts/Zombie2.cs
+++ Land of Dead/Assets/Scripts/Zombie2.cs
@@ -1,18 +1,126 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Zombie2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   [Header("Zombie Health and Damage")]
+    private float zombieHealth = 100f;
+    private float presentHealth;
+    public float giveDamage = 5f;
+    
+    [Header("Zombie Things")]
+    public NavMeshAgent zombieAgent;
+    public Transform LookPoint;
+    public Camera AttackingRaycastArea;
+    public Transform playerBody;
+    public LayerMask PlayerLayer;
+
+    [Header("Zombie Standing Var")]
+    public float zombieSpeed;
+
+    [Header("Zombie Attacking Var")]
+    public float timeBtwAttack;
+    bool previouslyAttack;
+
+    [Header("Zombie Animation")]
+    public Animator anim;
+
+
+    [Header("Zombie mood/states")]
+    public float visionRadius;
+    public float attackingRadius;
+    public bool playerInvisionRadius;
+    public bool playerInattackingRadius;
+
+    private void Awake()
     {
+        presentHealth = zombieHealth;
+        zombieAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() 
+    {
+        playerInvisionRadius = Physics.CheckSphere(transform.position, visionRadius, PlayerLayer);
+        playerInattackingRadius = Physics.CheckSphere(transform.position, attackingRadius, PlayerLayer);
+
+        if (!playerInvisionRadius && !playerInattackingRadius) Idle();
+        if (playerInvisionRadius && !playerInattackingRadius) Pursueplayer();
+        if (playerInvisionRadius && playerInattackingRadius) AttackPlayer();
+    }
+
+    private void Idle()
+    {
+        zombieAgent.SetDestination(transform.position);
+    }
+
+    private void Pursueplayer(){
+
+        if(zombieAgent.SetDestination(playerBody.position)){
+            //animations
+            //anim.SetBool("Walking", false);
+            //anim.SetBool("Running", true);
+            //anim.SetBool("Attacking", false);
+            //anim.SetBool("Died", false);
+        }
+        else{
+            //anim.SetBool("Walking", false);
+            //anim.SetBool("Running", false);
+            //anim.SetBool("Attacking", false);
+            //anim.SetBool("Died", true);
+        }
+
+    }
+    private void AttackPlayer(){
+        zombieAgent.SetDestination(transform.position);
+        transform.LookAt(LookPoint);
+        if(!previouslyAttack){
+            RaycastHit hitInfo;
+            if(Physics.Raycast(AttackingRaycastArea.transform.position, AttackingRaycastArea.transform.forward, out hitInfo, attackingRadius)){
+                Debug.Log("Attacking" + hitInfo.transform.name);
+
+                PlayerScript playerBody = hitInfo.transform.GetComponent<PlayerScript>();
+
+                if(playerBody != null)
+                {
+                    playerBody.playerHitDamage(giveDamage);
+                }
+                //anim.SetBool("Walking", false);
+                //anim.SetBool("Running", false);
+                //anim.SetBool("Attacking", true);
+                //anim.SetBool("Died", false);
+            }
+            previouslyAttack = true;
+            Invoke(nameof(ActiveAttacking), timeBtwAttack);
+        }
+    }
+    private void ActiveAttacking(){
+        previouslyAttack = false;
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void zombieHitDamage(float takeDamage){
+
+        presentHealth -= takeDamage;
+        if(presentHealth <= 0){
+            //anim.SetBool("Walking", false);
+            //anim.SetBool("Running", false);
+            //anim.SetBool("Attacking", false);
+            //anim.SetBool("Died", true);
+
+            zombieDie();
+        }
+
+    }
+
+    private void zombieDie(){
+        zombieAgent.SetDestination(transform.position);
+        zombieSpeed = 0f;
+        attackingRadius = 0f;
+        visionRadius = 0f;
+        playerInvisionRadius = false;
+        playerInattackingRadius = false;
+        Object.Destroy(gameObject,5.0f);
     }
 }
