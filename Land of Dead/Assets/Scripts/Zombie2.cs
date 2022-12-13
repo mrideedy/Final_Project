0ac19a6171ee04fefcd30de5162639e1a649d3a7
Aditya <aadi.avi990@gmail.com>
2022-12-13 12:27:05 +0530

    Initial Commit 53 (final)

--- Land of Dead/Assets/Scripts/Zombie2.cs
+++ Land of Dead/Assets/Scripts/Zombie2.cs
@@ -9,6 +9,7 @@ public class Zombie2 : MonoBehaviour
     private float zombieHealth = 100f;
     private float presentHealth;
     public float giveDamage = 5f;
+    public HealthBar healthBar;
     
     [Header("Zombie Things")]
     public NavMeshAgent zombieAgent;
@@ -37,6 +38,7 @@ public class Zombie2 : MonoBehaviour
     private void Awake()
     {
         presentHealth = zombieHealth;
+        healthBar.GiveFullHealth(zombieHealth);
         zombieAgent = GetComponent<NavMeshAgent>();
     }
 
@@ -99,6 +101,7 @@ public class Zombie2 : MonoBehaviour
     public void zombieHitDamage(float takeDamage){
 
         presentHealth -= takeDamage;
+        healthBar.SetHealth(presentHealth);
         if(presentHealth <= 0){
     
             anim.SetBool("Died", true);
