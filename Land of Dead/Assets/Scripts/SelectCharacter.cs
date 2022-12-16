0025c81f9c3799444a02f030d0f84cdbb9f4643a
vishal-grv <120037663+vishal-grv@users.noreply.github.com>
2022-12-17 00:31:16 +0530

    initial commit 62

---
+++ Land of Dead/Assets/Scripts/SelectCharacter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectCharacter : MonoBehaviour
+{
+    public void OnCharacter1()
+    {
+         SceneManager.LoadScene("LandOfTheDead");
+    }
+
+    public void OnCharacter2()
+    {   
+         SceneManager.LoadScene("LandOfTheDead");
+    }
+
+    public void OnCharacter3()
+    {
+         SceneManager.LoadScene("LandOfTheDead");
+    }
+}
+
+
+   
